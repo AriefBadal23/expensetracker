9e19ce72432d099599b7843666302427e8b6cef6
Arief Badal <ariefbadal21@outlook.com>
2025-12-10 18:47:26 +0100

    Add password field and isExpense field

--- expensetrackerapi/Models/User.cs
+++ expensetrackerapi/Models/User.cs
@@ -13,5 +13,6 @@ public record User
     public int Id { get; set; }
     public Role Role { get; set; }
     public required string Username { get; set; }
+    public required string Password { get; set; }
     public DateTime Created_at { get; set; } = DateTime.UtcNow;
 }
