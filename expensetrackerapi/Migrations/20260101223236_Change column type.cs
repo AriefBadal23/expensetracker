eacae464d85897f378a3566db767eb55e4dced5d
Arief Badal <ariefbadal21@outlook.com>
2026-01-03 07:17:01 +0100

    Migrations

---
+++ expensetrackerapi/Migrations/20260101223236_Change column type.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace expensetrackerapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changecolumntype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<LocalDate>(
+                name: "Created_at",
+                table: "Transactions",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created_at",
+                table: "Transactions",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(LocalDate),
+                oldType: "date");
+        }
+    }
+}
