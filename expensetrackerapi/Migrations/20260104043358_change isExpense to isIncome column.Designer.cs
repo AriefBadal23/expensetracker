bdc3e37798d62a31014779e4c23084669e72a75f
Arief Badal <ariefbadal21@outlook.com>
2026-01-04 13:24:02 +0100

    Replace isExpense to isIncome because that does not make sense

--- expensetrackerapi/Migrations/20260104043358_change isExpense to isIncome column.Designer.cs
+++ expensetrackerapi/Migrations/20260104043358_change isExpense to isIncome column.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NodaTime;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using expensetrackerapi.Models;
 namespace expensetrackerapi.Migrations
 {
     [DbContext(typeof(ExpenseTrackerContext))]
-    partial class ExpenseTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20260104043358_change isExpense to isIncome column")]
+    partial class changeisExpensetoisIncomecolumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ expensetrackerapi/Migrations/20260104043358_change isExpense to isIncome column.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace expensetrackerapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeisExpensetoisIncomecolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isExpense",
+                table: "Transactions",
+                newName: "IsIncome");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsIncome",
+                table: "Transactions",
+                newName: "isExpense");
+        }
+    }
+}
