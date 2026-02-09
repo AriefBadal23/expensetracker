86a7733a0c754681dd7c457bfe9de1033709925a
Arief Badal <ariefbadal21@outlook.com>
2026-02-09 09:33:13 +0100

    Add aggregration in BucketController.cs and make use of Service interface.

--- expensetrackerapi/Controllers/TransactionsController.cs
+++ expensetrackerapi/Controllers/TransactionsController.cs
@@ -38,39 +38,9 @@ namespace expensetrackerapi.Controllers
         [HttpPost]
         public ActionResult Post([FromBody] CreateTransactionDto dto)
         {
-            // We check first if the bucket exists at all
-            bool BucketExists = _db.Buckets.Any(x => x.Id == dto.BucketId);
 
-            // Make sure the input of the client is not negative and the bucket exists
-            if (dto.Amount > 0 && BucketExists)
-            {
-                // We get the salary bucket object.
-                Bucket Salary = _db.Buckets.First(x => x.Name == Buckets.Salary);
-
-                // Its necassary to update the Salary bucket total if it is an income otherwise it will substract from it.
-                Salary.Total = Salary.Total > 0 && dto.IsIncome == false ? Salary.Total - dto.Amount : Salary.Total + dto.Amount;
-
-                // Make sure we update the other bucket total amounts
-                Bucket TransactionBucket = _db.Buckets.First(x => x.Id == dto.BucketId);
-                TransactionBucket.Total = dto.Amount > 0 && dto.IsIncome == false ? TransactionBucket.Total + dto.Amount : TransactionBucket.Total;
-
-
-
-                _db.Buckets.UpdateRange([Salary, TransactionBucket]);
-
-                var transaction = new Transaction
-                {
-                    UserId = 1,
-                    BucketId = dto.BucketId,
-                    Description = dto.Description,
-                    Amount = dto.Amount,
-                    Created_at = dto.CreatedAt,
-                    IsIncome = dto.IsIncome
-                };
-                _db.Transactions.Add(transaction);
-                _db.SaveChanges();
-            }
-
+            var TransactionCreated = _expenseService.CreateTransaction(dto);
+            if (!TransactionCreated) return BadRequest();
             return Created();
 
         }
@@ -78,35 +48,10 @@ namespace expensetrackerapi.Controllers
         [HttpDelete("{transactionID}")]
         public ActionResult Delete(int transactionID)
         {
-            Transaction? t = _db.Transactions.FirstOrDefault(_ => _.Id == transactionID);
-            var transactionBucket = _db.Buckets.FirstOrDefault(_ => _.Id == t.BucketId);
-            Bucket Income = _db.Buckets.Where(_ => _.Name == Buckets.Salary).First();
-
-
-            if (t != null && transactionBucket != null)
+            var isDeleted = _expenseService.DeleteTransaction(transactionID);
+            if (isDeleted)
             {
-                _db.Transactions.Remove(t);
-                // Je kijkt of the transaction een income of expense is. 
-                // Daarop update je de Income total.
-                // -----
-                // Is the transaction not an income
-                // then add it back to the income and decrease the bucket amount.
-                // otherwise decrease it from the income and decrease it from the Income as well.
-
-                if (t.IsIncome is false)
-                {
-                    transactionBucket.Total -= t.Amount;
-                    Income.Total += t.Amount;
-                }
-                else
-                {
-                    Income.Total -= t.Amount;
-                }
-
-
-
-                _db.SaveChanges();
-                return NoContent();
+                return Ok();
             }
             return NotFound();
         }
--- expensetrackerapi/Services/ExpenseService.cs
+++ expensetrackerapi/Services/ExpenseService.cs
@@ -3,9 +3,9 @@ using expensetrackerapi.Models;
 public interface IExpenseService
 {
     public object GetTransactions(int? month, int? year, int? bucket, int pageNumber = 1, int pageSize = 3);
-    public void CreateTransaction();
+    public bool CreateTransaction(CreateTransactionDto dto);
 
-    public void DeleteTransaction();
+    public bool DeleteTransaction(int transactionID);
 
 
 }
@@ -103,13 +103,77 @@ namespace expensetrackerapi.Services
 
         }
 
-        public void CreateTransaction()
+        public bool CreateTransaction(CreateTransactionDto dto)
         {
+            // We check first if the bucket exists at all
+            bool BucketExists = _db.Buckets.Any(x => x.Id == dto.BucketId);
 
+            // Make sure the input of the client is not negative and the bucket exists
+            if (dto.Amount > 0 && BucketExists)
+            {
+                // We get the salary bucket object.
+                Bucket Salary = _db.Buckets.First(x => x.Name == Buckets.Salary);
+
+                // Its necassary to update the Salary bucket total if it is an income otherwise it will substract from it.
+                Salary.Total = Salary.Total > 0 && dto.IsIncome == false ? Salary.Total - dto.Amount : Salary.Total + dto.Amount;
+
+                // Make sure we update the other bucket total amounts
+                Bucket TransactionBucket = _db.Buckets.First(x => x.Id == dto.BucketId);
+                TransactionBucket.Total = dto.Amount > 0 && dto.IsIncome == false ? TransactionBucket.Total + dto.Amount : TransactionBucket.Total;
+
+
+
+                _db.Buckets.UpdateRange([Salary, TransactionBucket]);
+
+                var transaction = new Transaction
+                {
+                    UserId = 1,
+                    BucketId = dto.BucketId,
+                    Description = dto.Description,
+                    Amount = dto.Amount,
+                    Created_at = dto.CreatedAt,
+                    IsIncome = dto.IsIncome
+                };
+                _db.Transactions.Add(transaction);
+                _db.SaveChanges();
+                return true;
+            }
+            return false;
         }
 
-        public void DeleteTransaction()
+        public bool DeleteTransaction(int transactionID)
         {
+            Transaction? t = _db.Transactions.FirstOrDefault(_ => _.Id == transactionID);
+            var transactionBucket = _db.Buckets.FirstOrDefault(_ => t != null && _.Id == t.BucketId);
+            Bucket Income = _db.Buckets.Where(_ => _.Name == Buckets.Salary).First();
+
+
+            if (t != null && transactionBucket != null)
+            {
+                _db.Transactions.Remove(t);
+                // Je kijkt of the transaction een income of expense is. 
+                // Daarop update je de Income total.
+                // -----
+                // Is the transaction not an income
+                // then add it back to the income and decrease the bucket amount.
+                // otherwise decrease it from the income and decrease it from the Income as well.
+
+                if (t.IsIncome is false)
+                {
+                    transactionBucket.Total -= t.Amount;
+                    Income.Total += t.Amount;
+                }
+                else
+                {
+                    Income.Total -= t.Amount;
+                }
+
+
+
+                _db.SaveChanges();
+                return true;
+            }
+            return false;
 
         }
 
