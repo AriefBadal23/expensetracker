4440bf5d0b5c6b4806eae93671728394c85c52dc
Arief Badal <ariefbadal21@outlook.com>
2026-02-16 08:10:46 +0100

    Move interface and addition of icon and transactionType

---
+++ expensetrackerapi/Services/IExpenseService.cs
@@ -0,0 +1,13 @@
+﻿using expensetrackerapi;
+
+public interface IExpenseService
+{
+    public object GetTransactions(int? month, int? year, int? bucket, int pageNumber = 1, int pageSize = 3);
+    public bool CreateTransaction(CreateTransactionDto dto);
+
+    public bool DeleteTransaction(int transactionID);
+
+    public Transaction? GetTransactionByID(int Id);
+
+    public Transaction? UpdateTransaction(Transaction transaction);
+}
