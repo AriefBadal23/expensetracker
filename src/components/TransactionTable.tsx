fae18c0da0c45406cfcf7b30e24319886fa50d36
Arief Badal <ariefbadal21@outlook.com>
2026-02-16 08:08:30 +0100

    Add ability to update transaction. Change the behaviour of the modal so it makes use of React state.

--- src/components/TransactionTable.tsx
+++ src/components/TransactionTable.tsx
@@ -1,15 +1,37 @@
 import { type Dispatch, type SetStateAction } from "react";
 import type { Transaction } from "../types/Transaction";
 import TransactionRow from "./TransactionRow";
+import {useState} from "react";
+import CreateFormModal from "./CreateFormModal.tsx";
 
 interface TransactionTableProps {
   transactions: Transaction[];
   setTransactions: Dispatch<SetStateAction<Transaction[]>>
 }
 const TransactionTable = ({ transactions, setTransactions }: TransactionTableProps) => {
+  const [showModal, setShowModal] = useState(false);
   
   return (
     <>
+      <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => {
+            setShowModal(true)
+            console.log(showModal)
+
+          }}
+      >
+        Add new transaction
+      </button>
+
+      {
+        showModal ? 
+            <CreateFormModal SetShowModal={setShowModal} showModal={showModal} isUpdateForm={false}/>
+            : 
+            <>
+            </>
+      }
       <table className="table">
         <thead>
           <tr>
@@ -18,6 +40,7 @@ const TransactionTable = ({ transactions, setTransactions }: TransactionTablePro
             <th scope="col">Bucket</th>
             <th scope="col">Date</th>
             <th scope="col">Delete</th>
+            <th scope="col">Update</th>
           </tr>
         </thead>
         <tbody>
