d561bf874e80d6230d9c64fc7de2587ef0b15d25
Arief Badal <ariefbadal21@outlook.com>
2026-02-22 11:53:28 +0100

    Add DTO classes and Mapperly classes and config. And seperating the interface.

---
+++ expensetrackerapi/DTO/BucketResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using expensetrackerapi.Models;
+
+namespace expensetrackerapi.DTO;
+
+public record BucketResponseDto
+{
+    public int Month { get; set; }
+    public int Year { get; set; }
+    public List<BucketTransaction> Buckets { get; set; }
+
+    public int TotalIncome { get; set; }
+    public int TotalExpenses { get; set; }
+}
