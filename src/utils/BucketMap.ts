82ff0babafbbabeba4ab349fe922bf2f25ec8339
Arief Badal <ariefbadal21@outlook.com>
2026-02-27 22:16:01 +0100

    Change value order in BucketMap.ts record types

--- src/utils/BucketMap.ts
+++ src/utils/BucketMap.ts
@@ -2,12 +2,12 @@ import { Buckets } from "../types/Buckets";
 
 export const BucketToId: Record<Buckets, number> = {
   [Buckets.Salary]: 1,
-  [Buckets.Groceries]: 2,
-  [Buckets.Shopping]: 3,
+  [Buckets.Shopping]: 2,
+  [Buckets.Groceries]: 3,
 };
 
 export const IdToBucket: Record<number, Buckets> = {
   1: Buckets.Salary,
-  2: Buckets.Groceries,
-  3: Buckets.Shopping,
+  2: Buckets.Shopping,
+  3: Buckets.Groceries,
 };
