c1ddbbb2027bf6b918d98105a5ec6e3eea2ccd94
Arief Badal <ariefbadal21@outlook.com>
2026-03-02 23:30:02 +0100

    Add errror handling for fetching data of the API. - Make use of Error state - log the actual error message - Show generic error message to user in UI. - State for spinner and showing spinner by loading page.

--- src/components/BucketList.tsx
+++ src/components/BucketList.tsx
@@ -3,6 +3,7 @@ import Bucket from "./Bucket";
 import type { Bucket as BucketType } from "../types/Bucket";
 import { useEffect, useState } from "react";
 import type {Transaction} from "../types/Transaction.tsx";
+import {getErrorMessage} from "../utils/utils.ts";
 
 interface BucketListProps{
   transactions: Transaction[]
@@ -11,31 +12,47 @@ interface BucketListProps{
 
 const BucketList = ({transactions}:BucketListProps) => {
   const [buckets, SetBuckets] = useState<BucketType[]>([]);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [error, setError] = useState<string>();
+  const [isPending, setPending] = useState(true);
+
+  
+  const [error, setError] = useState<Error | undefined>();
 
 
   useEffect(() => {
     const fetchBuckets = async () => {
       try {
         const response = await fetch("http://localhost:5286/api/v1/buckets");
-        const data = await response.json();
-        SetBuckets(data);
-      } catch (error) {
-          if (error instanceof Error) {
-            console.log(error.name === TypeError.name)
-            console.log(error.message)
-            setError(error.message);
-          } else {
-            setError("Something went wrong with loading the data.");
-          }
+        
+        if(!response.ok){
+          return;
+        }
+          const data = await response.json()
+          SetBuckets(data);
+          setPending(false)
+        
+      } catch (err) {
+        setPending(false)
+        const message = getErrorMessage(err)
+        // 1. Log the actual error to the console.
+        // 2. Show an generic message in the UI for the user.
+        console.error(message)
+        setError(new Error("Failed to retrieve buckets data."))
       }
-    };
+    }
     fetchBuckets();
   }, [transactions]);
   return (
     <>
       <h1>Transaction Overview</h1>
+      
+      {isPending &&  <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      }
+
+      {error && <div>{error.message}</div>}
+
+      {!error &&
       <div className="bucket-list">
         {buckets !== null &&
           buckets.map((b: BucketType) => {
@@ -49,9 +66,10 @@ const BucketList = ({transactions}:BucketListProps) => {
             );
           })}
       </div>
+      }
     </>
   );
-};
+  };
 
 export default BucketList;
 // altijd een extra return wanneer je map() gebruikt;
--- src/components/TransactionTable.tsx
+++ src/components/TransactionTable.tsx
@@ -5,53 +5,61 @@ import {useState} from "react";
 import CreateFormModal from "./CreateFormModal.tsx";
 
 interface TransactionTableProps {
-  transactions: Transaction[];
-  setTransactions: Dispatch<SetStateAction<Transaction[]>>
+    transactions: Transaction[];
+    setTransactions: Dispatch<SetStateAction<Transaction[]>>
+    ErrorMessage: Error|undefined
 }
-const TransactionTable = ({ transactions, setTransactions }: TransactionTableProps) => {
-  const [showModal, setShowModal] = useState(false);
-
-  return (
+const TransactionTable = ({ transactions, setTransactions,ErrorMessage }: TransactionTableProps) => {
+    const [showModal, setShowModal] = useState(false);
+    return (
     <>
-      <button
-          type="button"
-          className="btn btn-primary"
-          onClick={() => {
-            setShowModal(true)
-
-          }}
-      >
-        Add new transaction
-      </button>
-
-      {
-          
-        showModal ? 
+        
+        {/*Show create modal*/}
+        { showModal ? 
             <CreateFormModal SetShowModal={setShowModal} showModal={showModal} isUpdateForm={false} setTransactions={setTransactions}/>
             : null
-      }
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Transaction</th>
-            <th scope="col">Amount</th>
-            <th scope="col">Bucket</th>
-            <th scope="col">Date</th>
-            <th scope="col">Delete</th>
-            <th scope="col">Update</th>
-          </tr>
-        </thead>
-        <tbody>
-        {
-            transactions.map((t) => {
-                return (
-                        <TransactionRow key={t.id} transaction={t} setTransactions={setTransactions} />
-                    )
-            })
-            
         }
-        </tbody>
-      </table>
+
+        {/*Show error message if any*/}
+        {ErrorMessage && <div>{ErrorMessage.message}</div>}
+        
+        {/*If no error message and data is fetched show table.*/}
+        {!ErrorMessage && transactions.length > 0  &&
+            <div>
+                <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={() => {    
+                        setShowModal(true)
+
+                    }}
+                >
+                    Add new transaction
+                </button>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Transaction</th>
+                        <th scope="col">Amount</th>
+                        <th scope="col">Bucket</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Delete</th>
+                        <th scope="col">Update</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        transactions.map((t) => {
+                            return (
+                                <TransactionRow key={t.id} transaction={t} setTransactions={setTransactions} /> 
+                            )
+                        })
+                    }
+                    </tbody>
+                </table>
+            </div>
+        
+        }
     </>
   );
 };
