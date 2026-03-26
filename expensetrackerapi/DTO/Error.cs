7f89d3056e3f1dda3224f2a373a266aedcbe3750
Arief Badal <ariefbadal21@outlook.com>
2026-03-26 11:43:12 +0100

    Add Error related types.

---
+++ expensetrackerapi/DTO/Error.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace HotelListing.Core.Models
+{
+    public class Error
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+
+}
+
+
+
