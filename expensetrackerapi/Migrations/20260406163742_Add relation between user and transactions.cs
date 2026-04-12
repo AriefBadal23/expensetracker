93f60481da23fda6983baf0f6cd45278b1b2cdd6
Arief Badal <ariefbadal21@outlook.com>
2026-04-12 14:06:25 +0200

    Add DbInitializer interface for refactored logic and migration files.

---
+++ expensetrackerapi/Migrations/20260406163742_Add relation between user and transactions.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace expensetrackerapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addrelationbetweenuserandtransactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "Created_at",
+                table: "Transactions",
+                newName: "CreatedAt");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "Transactions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_ApplicationUserId",
+                table: "Transactions",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_AspNetUsers_ApplicationUserId",
+                table: "Transactions",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_AspNetUsers_ApplicationUserId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_ApplicationUserId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "Transactions");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Transactions",
+                newName: "Created_at");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Transactions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
