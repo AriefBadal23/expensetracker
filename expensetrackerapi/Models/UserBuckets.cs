32b6422e4bfb21f8c0a7ac1078b0be5de79d8e0b
Arief Badal <ariefbadal21@outlook.com>
2026-04-13 16:51:07 +0200

    Add join entity type

---
+++ expensetrackerapi/Models/UserBuckets.cs
@@ -0,0 +1,10 @@
+﻿namespace expensetrackerapi.Models;
+
+public class UserBuckets
+{
+    // both FKs are automatically picked up and are configured as the composite PK for the join entity type.
+    // https://learn.microsoft.com/en-us/ef/core/modeling/relationships/many-to-many#many-to-many-with-class-for-join-entity
+    public string ApplicationUserId { get; set; }
+    public int BucketId { get; set; }
+    
+}
