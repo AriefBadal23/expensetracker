11d0f692fce2bf402fcd96d99e934c0d028e1bbe
Arief Badal <ariefbadal21@outlook.com>
2026-04-13 16:50:16 +0200

    Add migrations

---
+++ expensetrackerapi/Migrations/20260413053704_Add relation between buckets and users.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace expensetrackerapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addrelationbetweenbucketsandusers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Buckets_ApplicationUserId",
+                table: "Buckets",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Buckets_AspNetUsers_ApplicationUserId",
+                table: "Buckets",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Buckets_AspNetUsers_ApplicationUserId",
+                table: "Buckets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Buckets_ApplicationUserId",
+                table: "Buckets");
+        }
+    }
+}
