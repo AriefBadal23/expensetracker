04ed961bec3a4e780b71085358bd1e0d6bffa674
Arief Badal <ariefbadal21@outlook.com>
2026-04-14 20:11:43 +0200

    Add Updated Contracts/interfaces

--- expensetrackerapi/Contracts/IBucketService.cs
+++ expensetrackerapi/Contracts/IBucketService.cs
@@ -6,7 +6,7 @@ namespace expensetrackerapi.Contracts;
 
 public interface IBucketService
 {
-    public Task<Result<BucketResponseDto>> GetSummary(int month, int year);
+    public Task<Result<BucketResponseDto>> GetSummary(string userId, int month, int year);
     public Task<Result<List<Bucket>>> GetBuckets();
     public Task<Result<List<UserBucketResponseDto>>> GetBucketsByUserId(string? userId);
 }
--- expensetrackerapi/Contracts/IExpenseService.cs
+++ expensetrackerapi/Contracts/IExpenseService.cs
@@ -7,12 +7,12 @@ namespace expensetrackerapi.Contracts;
 
 public interface IExpenseService
 {
-    public Task<Result<object>> GetTransactions(string? userId, int? month, int? year, int? bucket, int pageNumber = 1, int pageSize = 3);
+    public Task<Result<object>> GetTransactions(string userId, int? month, int? year, int? bucket, int pageNumber = 1, int pageSize = 3);
     public Task<Result<ResponseTransactionDTo>> CreateTransaction(string userId, RequestTransactionDto transaction);
 
-    public Task<Result<bool>> DeleteTransaction(int transactionID);
+    public Task<Result<bool>> DeleteTransaction(string userId, int transactionID);
 
     public Task<Result<ResponseTransactionDTo?>> GetTransactionByID(int Id); // Task is enough; no async modifier required.
 
-    public Task<Result<ResponseTransactionDTo?>> UpdateTransaction(Transaction transaction);
+    public Task<Result<ResponseTransactionDTo?>> UpdateTransaction(string userId, int id, UpdateTransactionDto transaction);
 }
