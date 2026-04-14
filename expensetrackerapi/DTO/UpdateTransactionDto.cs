49e79e45a1701b6125c69f20fa610f0a333427f9
Arief Badal <ariefbadal21@outlook.com>
2026-04-14 20:16:01 +0200

    Fix createdAt property change and make use of new endpoint for user buckets

---
+++ expensetrackerapi/DTO/UpdateTransactionDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using expensetrackerapi.Validation;
+using NodaTime;
+
+namespace expensetrackerapi.DTO;
+
+public class UpdateTransactionDto
+{
+    public int BucketId { get; set; }
+    
+    [Required(ErrorMessage = "The Description is required.")]
+    [StringLength(50, ErrorMessage = "The description must be less than 50 characters.")]
+    public string? Description { get; set; }
+
+    [Range(0, 100_000, ErrorMessage = "The amount must be between 0 and 100_000.")]
+    public int Amount { get; set; }
+
+    [CreatedAtValidation]
+    [Required(ErrorMessage = "Created_at is required.")]
+    public LocalDate CreatedAt { get; set; }
+}
---
+++ expensetrackerapi/Migrations/20260414052613_UserBucket Total column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace expensetrackerapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserBucketTotalcolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Total",
+                table: "UserBuckets",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "UserBuckets");
+        }
+    }
+}
