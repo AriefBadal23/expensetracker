21a19ecc8063fd77d5d40fef7fef0913f3c51e2b
Arief Badal <ariefbadal21@outlook.com>
2026-06-15 20:37:56 +0200

    Add BucketRow and BucketTable components for displaying user buckets

---
+++ src/components/BucketRow.tsx
@@ -0,0 +1,40 @@
+﻿import type {Bucket} from "../types/Bucket.tsx";
+
+interface BucketRowProps{
+    bucket: Bucket
+}
+const BucketRow = ({bucket}:BucketRowProps) => {
+    
+    async function DeleteBucket(){
+        
+        try{
+            const URL = ""
+            await fetch(URL,
+                )
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+    
+    
+    return (
+        <tr key={bucket.bucket.id}>
+            <td>{bucket.bucket.name}</td>
+            <td>{bucket.bucket.type}</td>
+           
+            <td>
+                <button type="button"
+                        aria-label="Delete transaction">
+                    <img src="delete.png" alt="Delete transaction"/></button>
+            </td>
+            <td>
+                <button type="button"
+                        aria-label="Update transaction">
+                    <img src="update.png" alt="Update transaction"/></button>
+            </td>
+        </tr>
+    )
+}
+
+export default BucketRow;
---
+++ src/components/BucketTable.tsx
@@ -0,0 +1,54 @@
+﻿import {useEffect, useState} from "react";
+import type {Bucket} from "../types/Bucket.tsx";
+import BucketRow from "./BucketRow.tsx";
+
+const BucketTable = () => {
+    
+   const [buckets, setBuckets] = useState<Bucket[]>([])
+    // TODO: Error handling for failing api call
+    
+    useEffect(
+        // call fetch function HERE
+        () => {
+            async function fetchUserBuckets(){
+                const URL = "https://localhost:7118/api/v1/buckets/user"
+                try{
+                    const response = await fetch(URL, {
+                        method: "GET",
+                        credentials: "include"
+                    })
+                    const data = await response.json()
+                    setBuckets(data.value)
+                }
+                catch (err){
+                    console.log(err)
+                }
+            }
+         fetchUserBuckets()
+        },
+        []
+    )
+    return (
+        <div className="container mt-4">
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">Bucket</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Delete</th>
+                    <th scope="col">Update</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    buckets.map((bucket:Bucket) => (
+                        <BucketRow bucket={bucket} key={bucket.bucket.id}/>
+                    ))
+                }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default BucketTable;
