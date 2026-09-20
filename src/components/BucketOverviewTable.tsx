55b14cd077014d9555fccfdeaae43073833a1836
Arief Badal <ariefbadal21@outlook.com>
2026-09-20 16:20:04 +0200

    Fix bug with passing invalid month value type.

--- src/components/BucketOverviewTable.tsx
+++ src/components/BucketOverviewTable.tsx
@@ -19,7 +19,22 @@ const BucketOverviewTable = () => {
         month: currentMonth.toString(),
         year: currentYear.toString()
     })
-    const months: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    const months: { [key: string]: number } = {
+        "Januari": 1,
+        "Februari": 2,
+        "Maart": 3,
+        "April": 4,
+        "Mei": 5,
+        "Juni": 6,
+        "Juli": 7,
+        "Augustus": 8,
+        "September": 9,
+        "Oktober": 10,
+        "November": 11,
+        "December": 12
+    }
+    
     
     const [errorMessage, setErrorMessage] = useState<Error | undefined>()
     const [isPending, setPending] = useState<boolean>(true)
@@ -42,7 +57,6 @@ const BucketOverviewTable = () => {
         {
             try{
                 const url = `https://localhost:7118/api/v1/buckets/summary?month=${data.month}&year=${data.year}`
-                
                 const response = await fetch(url, {
                     credentials: "include"
                 })
@@ -101,7 +115,8 @@ const BucketOverviewTable = () => {
                                 }} id="selectedMonth">
                                     {
                                         // cleaner way to show the months as an option element
-                                        months.map((month) => <option key={month} value={month}>{month}</option>
+                                        Object.entries(months).map(([name, value]) => <option key={name}
+                                                                                              value={value}>{name}</option>
                                         )
                                     }
                                 </select>
